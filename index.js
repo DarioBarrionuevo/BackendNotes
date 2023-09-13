d32ce64fb52aa56b79d9de5ad657c7c4ac39b915
Dario Barrionuevo <dariobafran@gmail.com>
2023-09-13 11:25:55 +0200

    Added update endpoint

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 app.use(express.static("build"));
-
 let notes = [
   {
     id: 1,
@@ -51,9 +50,18 @@ app.get("/api/notes", (request, response) => {
 });
 // GET ONE NOTE
 app.get("/api/notes/:id", (request, response) => {
-  Note.findById(request.params.id).then((note) => {
-    response.json(note);
-  });
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      response.status(400).send({ error: "malformatted id" });
+    });
 });
 // CREATE NEW NOTE
 app.post("/api/notes", (request, response) => {
@@ -75,15 +83,45 @@ app.post("/api/notes", (request, response) => {
   });
 });
 // DELETE ONE NOTE
-app.delete('/api/notes/:id', (request, response, next) => {
+app.delete("/api/notes/:id", (request, response, next) => {
   Note.findByIdAndRemove(request.params.id)
-    .then(result => {
-      response.status(204).end()
+    .then((result) => {
+      response.status(204).end();
     })
-    .catch(error => next(error))
-})
+    .catch((error) => next(error));
+});
+// UPDATE ONE NOTE
+app.put("/api/notes/:id", (request, response, next) => {
+  console.log("🚀 ~ file: index.js:95 ~ app.put ~ request:", request.params.id)
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+      console.log("🚀 ~ file: index.js:106 ~ .then ~ updatedNote:", updatedNote)
+    })
+    .catch((error) => next(error));
+});
 
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+// this has to be the last loaded middleware.
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
