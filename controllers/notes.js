3f6cdec925f0a461170a59c2a948506a48dd9481
Dario Barrionuevo <dariobafran@gmail.com>
2023-09-20 15:47:07 +0200

    Add token auth

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,54 +1,54 @@
 /* eslint-disable quotes */
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
-
+const User = require("../models/user");
 // GET ALL
-// notesRouter.get('/', (request, response) => {
-//   Note.find({}).then((notes) => {
-//     response.json(notes);
-//   });
-// });
 notesRouter.get("/", async (request, response) => {
-  const notes = await Note.find({});
+  const notes = await Note.find({}).populate("user", { username: 1, name: 1 });
   response.json(notes);
 });
 // GET ONE
-notesRouter.get('/:id', async (request, response) => {
-  const note = await Note.findById(request.params.id)
+notesRouter.get("/:id", async (request, response) => {
+  const note = await Note.findById(request.params.id);
   if (note) {
-    response.json(note)
+    response.json(note);
   } else {
-    response.status(404).end()
+    response.status(404).end();
   }
-})
+});
 
 // ADD ONE
 notesRouter.post("/", async (request, response, next) => {
   const body = request.body;
+  const user = await User.findById(body.userId);
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
+    user: user._id,
   });
 
   try {
     const savedNote = await note.save();
+    user.notes = user.notes.concat(savedNote._id);
+    await user.save();
+
     response.json(savedNote);
   } catch (exception) {
     next(exception);
   }
 });
 // DELETE ONE
-notesRouter.delete('/:id', async (request, response, next) => {
+notesRouter.delete("/:id", async (request, response, next) => {
   try {
-    await Note.findByIdAndRemove(request.params.id)
-    response.status(204).end()
+    await Note.findByIdAndRemove(request.params.id);
+    response.status(204).end();
   } catch (exception) {
-    next(exception)
+    next(exception);
   }
-})
-
+});
+// UPDATE ONE
 notesRouter.put("/:id", (request, response, next) => {
   const body = request.body;
 
@@ -56,7 +56,7 @@ notesRouter.put("/:id", (request, response, next) => {
     content: body.content,
     important: body.important,
   };
-  // UPDATE ONE
+
   Note.findByIdAndUpdate(request.params.id, note, { new: true })
     .then((updatedNote) => {
       response.json(updatedNote);
